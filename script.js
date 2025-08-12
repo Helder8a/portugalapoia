5297947f3cb2faf0cace18405d7e75a9ef18a640
helder <100797030+Helder8a@users.noreply.github.com>
2025-08-12 22:48:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -60,3 +60,16 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+// --- Lógica de Registro del Service Worker para PWA ---
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then(registration => {
+                console.log('Service Worker registrado con éxito:', registration);
+            })
+            .catch(error => {
+                console.log('Error al registrar el Service Worker:', error);
+            });
+    });
+}
