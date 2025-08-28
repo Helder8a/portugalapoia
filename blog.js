4661faba34235d3ec7c4d83e8d23092d0099a93d
helder <100797030+Helder8a@users.noreply.github.com>
2025-08-28 10:12:59 +0100

    Add files via upload

--- blog.js
+++ blog.js
@@ -1,14 +1,19 @@
-// Reemplaza esta función en tu fichero blog.js
+// ==================================================================
+// === FUNCIÓN CORREGIDA Y DEFINITIVA para blog.js ===
+// ==================================================================
 
 function renderGalleryItem(item) {
-    // Añadimos un div contenedor con las clases de columna para asegurar la compatibilidad con Bootstrap
+    // Se genera una estructura de columna de Bootstrap limpia (col-lg-4 col-md-6)
+    // El enlace <a> (con la clase .gallery-item) contiene todo el diseño.
     return `
-    <div class="col-lg-4 col-md-6 mb-4 gallery-item-wrapper">
+    <div class="col-lg-4 col-md-6 mb-4">
         <a href="${item.image}" data-lightbox="gallery" data-title="${item.title} - ${item.caption}" class="gallery-item">
-            <img src="${item.image}" alt="${item.title}">
-            <div class="caption-overlay">
-                <h3 class="gallery-title">${item.title}</h3>
-                <p class="gallery-caption">${item.caption}</p>
+            <img class="gallery-item-img" src="${item.image}" alt="${item.title}">
+            <div class="gallery-item-overlay">
+                <div class="gallery-item-text">
+                    <h3 class="gallery-item-title">${item.title}</h3>
+                    <p class="gallery-item-caption">${item.caption}</p>
+                </div>
             </div>
         </a>
     </div>`;
